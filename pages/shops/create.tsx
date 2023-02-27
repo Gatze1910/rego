8567f34a6465f849d1614346dc670e2cb9a1e144
Bernadette Ackerl <backerl.lba@fh-salzburg.ac.at>
2023-02-27 09:24:17 +0100

    Add dummy picture upload

--- pages/shops/create.tsx
+++ pages/shops/create.tsx
@@ -1,6 +1,7 @@
-import ProtectedRoute from '../../components/basic/ProtectedRoute'
 import { useUser } from '@auth0/nextjs-auth0/client'
 import { gql, useMutation } from '@apollo/client'
+import { ChangeEvent, useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 const ADD_SHOP = gql`
   mutation createShop(
     $name: String!
@@ -24,17 +25,49 @@ const ADD_SHOP = gql`
 `
 
 const CreateShop = () => {
-  const user = useUser()
+  const { user } = useUser()
+  const supabase = createBrowserSupabaseClient()
+  const [image, setImage] = useState(null)
+
+  const handleImageUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    let file;
+
+    if (e.target.files) {
+      file = e.target.files[0];
+    }
+
+    const { data, error } = await supabase.storage
+      .from("shop")
+      .upload("public/" + file?.name, file as File);
+
+    if (data) {
+      console.log(data);
+      let path = data.path
+      setImage("https://rvplealboqicxmexeqdd.supabase.co/storage/v1/object/public/shop/public/Contacta-2022-Web-238.jpg")
+      
+
+    } else if (error) {
+      console.log(error);
+    }
+  };
   
-
-  if (!user) {
-    window.location.href = 'localhost:3000'
-  }
-
 
   const [shop, { data, loading, error }] = useMutation(ADD_SHOP)
   return (
     <>
+    <h1>image upload</h1>
+    <input
+        type="file"
+        accept="image/*"
+        className="block w-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        id="file_input"
+        onChange={(e) => {
+          handleImageUpload(e); 
+        }}
+      />
+      {image &&  <img src={image} />}
+           
+
       <h2>You are logged in! You are able to create a shop</h2>
       <div style={{ ['margin-top' as any]: '50vh' }}>
         <form
@@ -56,6 +89,7 @@ const CreateShop = () => {
           <button type="submit">Add Shop zum Test</button>
         </form>
       </div>
+      <div></div>
     </>
   )
 }
