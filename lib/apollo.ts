eeeb573e8418010974e36f697a5c6cbbce50dceb
Gatze1910 <58788845+Gatze1910@users.noreply.github.com>
2023-03-01 11:18:50 +0100

    Update apollo.ts
    update apolloclient uri

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,15 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
-  cache: new InMemoryCache(),
-})
+if (process.env.NODE_ENV === 'production') {
+  const apolloClient = new ApolloClient({
+    uri: 'https://rego-tau.vercel.app/api/graphql',
+    cache: new InMemoryCache(),
+  })
+} else {
+  const apolloClient = new ApolloClient({
+    uri: 'http://localhost:3000/api/graphql',
+    cache: new InMemoryCache(),
+  })
+}
 
 export default apolloClient
