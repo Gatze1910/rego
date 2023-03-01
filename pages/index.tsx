dc8ba8a6c738b204cb9d015f0da2b259b026554c
Bernadette Ackerl <backerl.lba@fh-salzburg.ac.at>
2023-03-01 09:26:23 +0100

    Removed firebase for auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,64 +38,10 @@ export const Home = () => {
 
         <div>
           <p>wir sind gerade beim Entwicklen von</p>
-          <h1>ReGo - Regionalität to go</h1>
+          <h1>ReGo - Regional to go</h1>
           <p>besuche uns bald wieder</p>
         </div>
-        <form className="uk-form">
-          <div>
-            <label className="uk-form-label">Text</label>
-            <div className="uk-inline">
-              <span className="uk-form-icon" uk-icon="icon: user"></span>
-              <input
-                className="uk-input uk-form-danger"
-                type="text"
-                placeholder="Text"
-              />
-            </div>
-          </div>
-          <div>
-            <label className="uk-form-label">E-Mail</label>
-            <input
-              className="uk-input"
-              type="email"
-              placeholder="E-Mail"
-              disabled
-            />
-          </div>
-          <div>
-            <label className="uk-form-label">Number</label>
-            <input
-              className="uk-form-controls uk-input"
-              type="number"
-              placeholder="10"
-            />
-          </div>
-          <textarea className="uk-textarea uk-form-danger"></textarea>
-          <input type="radio" name="test" value="test" className="uk-radio" />
-          <input type="radio" name="test" value="test" className="uk-radio" />
-          <input type="checkbox" name="test" className="uk-checkbox" />
-          <div uk-form-custom="target: true">
-            <input type="file" aria-label="Custom controls" />
-            <input
-              className="uk-input"
-              type="text"
-              placeholder="Select file"
-              aria-label="Custom controls"
-            />
-          </div>
-          <ButtonPrimary
-            onClick={() => {
-              alert('funktioniert!')
-            }}
-          >
-            Button
-          </ButtonPrimary>
-          <ButtonSecondary disabled={true}>Button</ButtonSecondary>
-          <select className="uk-select">
-            <option value="test">Test</option>
-            <option value="test">Test</option>
-          </select>
-        </form>
+     
       </main>
     </>
   )
